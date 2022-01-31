8e4ed1f73e7300fc3b04be425925334ad05c0f20
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-01-31 15:54:27 -0600

    fixed issue in combined arrays

--- data/testLogic/index.ts
+++ data/testLogic/index.ts
@@ -458,17 +458,15 @@ export const testScripts = {
       result: JSON.stringify(combineArrs([1, 3, 5], [2, 4, 6])),
     });
     testResults.push({
-      test: 'combineArrs(["dogs", "cats", "hampsters"], ["snakes", "rats", "chickens"]) returns ['cats', 'chickens', 'dogs', 'hampsters', 'rats', 'snakes']',
+      test: 'combineArrs(["dogs", "cats", "hampsters"], ["snakes", "rats", "chickens"]) returns ["cats", "chickens", "dogs", "hampsters", "rats", "snakes"]',
       passed:
-        JSON.stringify(combineArrs(['dogs', 'cats', 'hampsters'], ['snakes', 'rats', 'chickens'])) ===
-        JSON.stringify(['cats', 'chickens', 'dogs', 'hampsters', 'rats', 'snakes']),
+        JSON.stringify(combineArrs(['dogs', 'cats', 'hampsters'], ['snakes', 'rats', 'chickens'])) === JSON.stringify(['cats', 'chickens', 'dogs', 'hampsters', 'rats', 'snakes']),
       result: JSON.stringify(combineArrs(['dogs', 'cats', 'hampsters'], ['snakes', 'rats', 'chickens'])),
     });
     testResults.push({
       test: 'combineArrs([5, -3, -1, 8, 2], [10, 9, -2, -4, 7]) returns [-4, -3, -2, -1, 2, 5, 7, 8, 9, 10]',
       passed:
-        JSON.stringify(combineArrs([5, -3, -1, 8, 2], [10, 9, -2, -4, 7])) ===
-        JSON.stringify([-4, -3, -2, -1, 2, 5, 7, 8, 9, 10]),
+        JSON.stringify(combineArrs([5, -3, -1, 8, 2], [10, 9, -2, -4, 7])) === JSON.stringify([-4, -3, -2, -1, 2, 5, 7, 8, 9, 10]),
       result: JSON.stringify(combineArrs([5, -3, -1, 8, 2], [10, 9, -2, -4, 7])),
     });
     testResults.push({
