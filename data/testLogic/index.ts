44202d58a00e40366e4eebf10514b3aeaa5915ce
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-01-31 15:41:16 -0600

    fixed typo in combine arrays problem

--- data/testLogic/index.ts
+++ data/testLogic/index.ts
@@ -458,14 +458,14 @@ export const testScripts = {
       result: JSON.stringify(combineArrs([1, 3, 5], [2, 4, 6])),
     });
     testResults.push({
-      test: 'combineArrs(["dogs", "cats", "hampsters"], ["snakes", "rats", "chickens"]) returns [1, 2, 3, 4, 5, 6]',
+      test: 'combineArrs(["dogs", "cats", "hampsters"], ["snakes", "rats", "chickens"]) returns ['cats', 'chickens', 'dogs', 'hampsters', 'rats', 'snakes']',
       passed:
         JSON.stringify(combineArrs(['dogs', 'cats', 'hampsters'], ['snakes', 'rats', 'chickens'])) ===
         JSON.stringify(['cats', 'chickens', 'dogs', 'hampsters', 'rats', 'snakes']),
       result: JSON.stringify(combineArrs(['dogs', 'cats', 'hampsters'], ['snakes', 'rats', 'chickens'])),
     });
     testResults.push({
-      test: 'combineArrs([1, 3, 5], [2, 4, 6]) returns [1, 2, 3, 4, 5, 6]',
+      test: 'combineArrs([5, -3, -1, 8, 2], [10, 9, -2, -4, 7]) returns [-4, -3, -2, -1, 2, 5, 7, 8, 9, 10]',
       passed:
         JSON.stringify(combineArrs([5, -3, -1, 8, 2], [10, 9, -2, -4, 7])) ===
         JSON.stringify([-4, -3, -2, -1, 2, 5, 7, 8, 9, 10]),
