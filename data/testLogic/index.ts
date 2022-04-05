71c1e74319fd7c01a02efa60c87715cc3443ee79
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-04-05 00:32:44 -0500

    reformatted test script for even odd

--- data/testLogic/index.ts
+++ data/testLogic/index.ts
@@ -1,44 +1,9 @@
 import { sumTwoIntTests } from './testScripts/sumTwoInt';
+import { evenOrOddTests } from './testScripts/evenOrOdd';
 
 export const testScripts = {
   sumTwoIntsTestScript: `const runTests = ${sumTwoIntTests.toString()} \n runTests();`,
-  evenOrOddTestScript: `const runTests = () => {
-    const testResults = [];
-    testResults.push({
-      test: 'User created a function evenOrOdd.',
-      passed: typeof evenOrOdd === 'function',
-      result: typeof evenOrOdd,
-    });
-    testResults.push({
-      test: 'Function returns a string.',
-      passed: typeof evenOrOdd(11) === 'string',
-      result: typeof evenOrOdd(11),
-    });
-    testResults.push({
-      test: 'evenOrOdd(11) returns "odd"',
-      passed: evenOrOdd(11) === 'odd',
-      result: evenOrOdd(11),
-    });
-    testResults.push({
-      test: 'evenOrOdd(30) returns "even"',
-      passed: evenOrOdd(30) === 'even',
-      result: evenOrOdd(30),
-    });
-    testResults.push({
-      test: 'evenOrOdd(17) returns "odd"',
-      passed: evenOrOdd(17) === 'odd',
-      result: evenOrOdd(17),
-    });
-    testResults.push({
-      test: 'evenOrOdd(22) returns "even"',
-      passed: evenOrOdd(22) === 'even',
-      result: evenOrOdd(22),
-    });
-  
-    return testResults;
-  };
-  
-  runTests();`,
+  evenOrOddTestScript: `const runTests = ${evenOrOddTests.toString()} \n runTests();`,
   rockPaperScissorsTestScript: `const runTests = () => {
     const testResults = [];
     testResults.push({
